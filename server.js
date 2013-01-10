a4882316d628c822244961c5e2a5bdda7ea517c9
Christophe Beveraggi <beve@localhost.localdomain>
2013-01-10 19:27:02 +0100

    - Rewrite server side in pure js

--- server.js
+++ server.js
@@ -1,23 +1,67 @@
+var fs = require('fs');
+var stylus = require('stylus');
+var express = require('express');
+var photos = require('./src/captain/photos');
+var app = express();
 
-var initModule = "captain/main";
+var photosList = null;
 
-dojoConfig = {
-  hasCache: {
-    "host-node": 1,
-    "dom": 0,
-    "dojo-built": 1
-  },
-  trace: 1,
-  async: 1,
-  baseUrl: "src/",
-  packages: [{
-    name: "dojo",
-    location: "dojo"
-  },{
-    name: "captain",
-    location: "captain"
-  }],
-  deps: [initModule]
+// Config
+var configFile = 'config/config.json';
+var config;
+if (!fs.existsSync(configFile)) {
+  console.error('Config file '+configFile+' not found. Please create it from config.js.distfile');
+  process.exit(1);
+} else {
+  try {
+    config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+  } catch (e) {
+    console.error('Malformed config file: '+e.message);
+    process.exit(1);
+  }
 }
 
-require("./src/dojo/dojo.js");
+function compile(str, path){
+  return stylus(str).
+  set("filename", path).
+  use(nib());
+}
+
+app.configure(function(){
+  app.set('views', 'views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon('public/img/favicon.ico'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser(config.secret));
+  app.use(express.static('public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(app.router);
+  app.use(stylus.middleware({
+    src: 'views',
+    dest: 'public',
+    compile: compile,
+    compress: true
+  }));
+});
+
+app.get('/', function(req, res) {
+  res.render('index', {});
+});
+
+app.get('/photos', function(req, res) {
+  if (photosList === null) {
+    var photosLib = new photos();
+    photosLib.find('public/Photos', function(err, p) {
+      if (err) throw err;
+      photosList = {photos: p};
+      res.json(photosList);
+    });
+  } else {
+    res.json(photosList);
+  }
+});
+
+// Start server
+app.listen(config.server.port);
+console.log('Server ready');
--- src/captain/photos.js
+++ src/captain/photos.js
@@ -1,37 +1,38 @@
-define([
-		"dojo/node!fs",
-		"dojo/node!path",
-		"dojo/_base/array",
-		"dojo/_base/declare"
-	],
-	function(fs, path, array, declare) {
-		var photos = declare(null, {});
+var fs = require('fs');
+var path = require('path');
 
-		photos.find = function(dir, done) {
-			var results = [];
-			fs.readdir(dir, function(err, list) {
-				if (err) return done(err);
-				var pending = list.length;
-				if (!pending) return done(null, results);
-				list.forEach(function(file) {
-					file = dir + '/' + file;
-					fs.stat(file, function(err, stat) {
-						if (stat && stat.isDirectory()) {
-							photos.find(file, function(err, res) {
-								results = results.concat(res);
-								if (!--pending) done(null, results);
-							});
-						} else {
-							if ((file.indexOf('_min') != -1) && array.indexOf(['jpg', 'png', 'gif'], path.extname(file).toLowerCase())) {
-								results.push(file.replace('public/', ''));
-							}
+var photos = module.exports = function photos() {
+	this.path = '';
+	this.photosList = null;
+	this.cache = {};
+};
+
+photos.prototype = {
+
+	find: function(dir, done) {
+		var self = this;
+		var results = [];
+		fs.readdir(dir, function(err, list) {
+			if (err) return done(err);
+			var pending = list.length;
+			if (!pending) return done(null, results);
+			list.forEach(function(file) {
+				file = dir + '/' + file;
+				fs.stat(file, function(err, stat) {
+					if (stat && stat.isDirectory()) {
+						self.find(file, function(err, res) {
+							results = results.concat(res);
 							if (!--pending) done(null, results);
+						});
+					} else {
+						if ((file.indexOf('_min') != -1) && (['.jpg', '.png', '.gif'].indexOf(path.extname(file).toLowerCase()) != -1)) {
+							results.push(file.replace('public/', ''));
 						}
-					});
+						if (!--pending) done(null, results);
+					}
 				});
 			});
-		}
-
-		return photos;
+		});
 	}
-);
+
+};
