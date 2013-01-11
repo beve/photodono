e03ce014166081422ecd5d0f83285cca1f31facd
Christophe Beveraggi <beve@localhost.localdomain>
2013-01-11 16:59:31 +0100

    - File listing async ok

--- src/captain/photos.js
+++ src/captain/photos.js
@@ -2,21 +2,17 @@ var fs = require('fs');
 var path = require('path');
 
 var photos = module.exports = function photos() {
-	this.path = '';
-	this.photosList = {};
-	this.cache = {};
+	this.list = {};
 };
 
 photos.prototype = {
 
 	find: function(dir, done) {
 		var self = this;
-		console.log('ici');
-		fs.readdir(dir, function(err, list) {
-			if (err) return done(err);
-			var pending = list.length;
+		fs.readdir(dir, function(err, fList) {
+			var pending = fList.length;
 			if (!pending) return done(null);
-			list.forEach(function(file) {
+			fList.sort().forEach(function(file) {
 				file = dir + '/' + file;
 				fs.stat(file, function(err, stat) {
 					if (stat && stat.isDirectory()) {
@@ -24,31 +20,28 @@ photos.prototype = {
 							if (!--pending) done(null);
 						});
 					} else {
-						// add file
-						var current = self.photosList;
-						if ((file.indexOf('_min') != -1) && (['.jpg', '.png', '.gif'].indexOf(path.extname(file).toLowerCase()) != -1)) {
-							file.replace('public/Photos/', '').split(path.sep).forEach(function(element, index, array) {
-								if (index < array.length-1) {
-									if (!current[element]) {
-										current[element] = {};
-									}
-									current = current[element];
-								} else {
-									if (!current['img']) {
-										current['img'] = [];
-									}
-									console.log(element);
-									current['img'].push(element);
-								}
-
-							});
-							//self.photosList.push();
-						}
+						self.populateFileList(file);
 						if (!--pending) done(null);
 					}
 				});
 			});
 		});
+	},
+
+	populateFileList: function(file) {
+		var current = this.list;
+		if ((file.indexOf('_min') != -1) && (['.jpg', '.png', '.gif'].indexOf(path.extname(file).toLowerCase()) != -1)) {
+			file.replace('public/Photos/', '').split(path.sep).forEach(function(element, index, array) {
+				if (index < array.length-1) {
+					if (!current[element]) current[element] = {};
+					current = current[element];
+				} else {
+					if (!current['img']) current['img'] = [];
+					current['img'].push(element);
+				}
+
+			});
+		}
 	}
 
 };
