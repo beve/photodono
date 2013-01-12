debbb238c7c81e25323bbcf536065bea3fe2ca31
Christophe Beveraggi <beve@localhost.localdomain>
2013-01-12 02:40:58 +0100

    removed jquery tests

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,6 +1,18 @@
-require(['/js/photos.js', 'dojo/domReady!'], function(photos, on) {
+define.amd.jQuery = true;
+require(['/js/photos.js', 'dojo/dom', 'jquery', 'jquery!imagesloaded', 'dojo/domReady!'], function(photos, dom, jQuery) {
   var p = new photos();
-  p.getList(function(err, images) {
-    console.log(images);
+  p.getList(function(err) {
+    if (!err) {
+      var f = p.getFromPath('02_Sport.01_VTT');
+      console.log(f);
+    }
   });
+
+console.log('ici');
+  var toto = jQuery('#viewport').imagesLoaded(function(i, p, b) {
+
+  });
+  //console.log(toto.imagesLoaded);
+
+
 });
--- public/js/photos.js
+++ public/js/photos.js
@@ -1,20 +1,27 @@
-define(['dojo/_base/declare', 'dojo/request', 'dojo/json', 'dojo/Evented'], function(declare, request, JSON, Evented) {
+define(['dojo/_base/declare', 'dojo/request', 'dojo/json', 'dojo/_base/lang', 'dojo/Evented'], function(declare, request, JSON, lang, Evented) {
 
   return declare([Evented], {
 
     constructor: function () {
-      this.photosList = [];
+      this.list = [];
     },
 
     getList: function(callback) {
+      var self = this;
       request("/photos", {handleAs:'json'}).then(
          function(list) {
-           callback(null, list);
+           self.list = list;
+           callback(null);
          },
          function(err) {
            console.log(err); 
          }
        );
+     },
+
+     getFromPath: function(dottedPath) {
+        var list = lang.getObject(dottedPath, false, this.list);
+        return list; 
      }
 
    });
