d6f51fc0b45f41f831d08b05a7d113ac640314e9
Christophe Beveraggi <beve@localhost.localdomain>
2013-01-12 19:10:18 +0100

    - IMproved module declaration

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,12 +1,17 @@
-define.amd.jQuery = true;
-require(['/js/photos.js', 'dojo/dom', 'dojo/domReady!'], function(photos, dom, jQuery) {
+require(['photos', 'dojo/dom', "dijit/Dialog", 'dojo/domReady!'], function(photos, dom, Dialog) {
   var p = new photos();
   p.getList(function(err) {
     if (!err) {
+       myDialog = new Dialog({
+            title: "My Dialog",
+            content: "Test content.",
+            style: "width: 300px"
+        });
+       myDialog.show();
+
       var f = p.getFromPath('02_Sport.01_VTT');
       console.log(f);
     }
   });
-
 
 });
