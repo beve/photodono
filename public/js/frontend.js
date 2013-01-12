4854d277689c1f86fb76963fd5f6f80ee6c24256
Christophe Beveraggi <beve@localhost.localdomain>
2013-01-12 02:59:41 +0100

    - Welcome back dijit !

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,5 +1,5 @@
 define.amd.jQuery = true;
-require(['/js/photos.js', 'dojo/dom', 'jquery', 'jquery!imagesloaded', 'dojo/domReady!'], function(photos, dom, jQuery) {
+require(['/js/photos.js', 'dojo/dom', 'dojo/domReady!'], function(photos, dom, jQuery) {
   var p = new photos();
   p.getList(function(err) {
     if (!err) {
@@ -7,12 +7,6 @@ require(['/js/photos.js', 'dojo/dom', 'jquery', 'jquery!imagesloaded', 'dojo/dom
       console.log(f);
     }
   });
-
-console.log('ici');
-  var toto = jQuery('#viewport').imagesLoaded(function(i, p, b) {
-
-  });
-  //console.log(toto.imagesLoaded);
 
 
 });
